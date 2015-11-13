cfbfa85886c082a84cfd96e4602a9c45f4124711
marco <marco.v.correia@gmail.com>
2015-11-13 22:46:22 +0000

    FIX: symbolic links not working

--- kangrouter.py
+++ kangrouter.py
@@ -1,1 +1,88 @@
-../__init__.py
+import time
+
+from tsm.common.app import exception
+
+import requests
+import json
+
+class KangRouterClient:
+  pathbase = "https://thesolvingmachine.com/kangrouter/srv/v1/solvers"
+  def __init__(self,apiKey,licenseId=None):    
+    self.apiKey = apiKey
+    self.licenseId = licenseId
+
+  def validateReply(self,req):
+    if req.status_code >= 400 and req.status_code <= 500:
+      try:
+        j = req.json()
+      except ValueError:
+        raise exception.InternalError(req.text,req.status_code)
+      raise exception.jsonToException(req.json())
+
+  def create(self,problem,**kwargs):
+    path = self.pathbase
+    payload=json.dumps(problem)
+    params = self.getCommonParams().copy()
+    params.update(kwargs)
+    req = requests.post(path,
+                        params=params, 
+              headers={'content-type': 'application/json'},
+              data=payload)    
+    self.validateReply(req)
+    return req.json()["solverId"]
+    
+  def delete(self,solverId):
+    path = "{base}/{solverId}".format(base=self.pathbase,
+                                      solverId=str(solverId))
+    req = requests.delete(path,
+                          params=self.getCommonParams())
+    self.validateReply(req)
+    return True
+    
+  def stop(self,solverId):
+    path = "{base}/{solverId}/stop".format(base=self.pathbase,
+                                      solverId=str(solverId))
+    req = requests.put(path,
+                       params=self.getCommonParams())
+    self.validateReply(req)
+    return True
+    
+  def getStatus(self,solverId):
+    path = "{base}/{solverId}/status".format(base=self.pathbase,
+                                      solverId=str(solverId))
+    req = requests.get(path,
+                       params=self.getCommonParams())
+    self.validateReply(req)
+    return req.json()
+
+  def getSolution(self,solverId):
+    path = "{base}/{solverId}/solution".format(base=self.pathbase,
+                                      solverId=str(solverId))
+    req = requests.get(path,
+                       params=self.getCommonParams())
+    self.validateReply(req)
+    return req.json()
+    
+  # polling 
+  def createAndWait(self,problem,cancel,**kwargs):
+    solverId = self.create(problem,**kwargs)
+    timeout = 300
+    while not cancel() and timeout>0:
+      status = self.getStatus(solverId)
+      if status["execStatus"] =="invalid":
+        raise exception.solverError(json.dumps(status["errors"]))
+      if status["execStatus"] =="completed":
+        return self.getSolution(solverId)
+      time.sleep(1)
+      timeout -= 1
+    if timeout == 0:
+      raise exception.InternalError("Timed out waiting for solver")
+    raise exception.UserCancelled()
+    
+  def getCommonParams(self):
+    if self.licenseId:
+      return {"licenseId":self.licenseId, "apiKey":self.apiKey}
+    else:
+      return {"apiKey":self.apiKey}
+
+    
--- tsm/common/app/exception.py
+++ tsm/common/app/exception.py
@@ -1,1 +1,89 @@
-../../../../../../../../common/app/exception.py
+
+ERROR_INVALID_API_USAGE = 1
+ERROR_INTERNAL = 2
+ERROR_USER_CANCELLED = 3
+ERROR_GEOCODER = 4
+
+errors = { ERROR_INVALID_API_USAGE : "Invalid API usage.",
+       ERROR_INTERNAL : "Internal error.",
+       ERROR_USER_CANCELLED : "Cancelled by user.",
+       ERROR_GEOCODER : "Geocoder error."}
+
+class MultipleInvalidAPIUsage(Exception):
+  pass
+
+class InvalidAPIUsage(Exception):
+  status_code = 400
+  code = ERROR_INVALID_API_USAGE
+  def __init__(self, message, status_code=None, payload=None):
+    Exception.__init__(self)
+    self.message = message
+    if status_code is not None:
+      self.status_code = status_code
+    self.payload = payload
+
+  def format(self,**data):
+    self.message = self.message.format(**data)
+    return self
+  
+  def to_dict(self, showXInfo=True):
+    rv = {"code" : self.code,
+        "error" : errors[ERROR_INVALID_API_USAGE],
+        "info" : self.message}
+    return rv
+  
+  def __str__(self):
+    return "Invalid API Usage: "+self.message+" (status_code="+str(self.status_code)+")"
+  
+class InternalError(Exception):
+  code = ERROR_INTERNAL
+  def __init__(self, message,status_code=500):
+    Exception.__init__(self)
+    self.status_code = status_code
+    self.message = message
+  def to_dict(self):
+    rv = {"code" : self.code,
+        "error" : errors[ERROR_INTERNAL],
+        "info" : self.message}
+    return rv
+  def __str__(self):
+    return "Internal Error ("+str(self.status_code)+") :"+str(self.message)
+  
+class UserCancelled(Exception):
+  code = ERROR_USER_CANCELLED
+  def __init__(self, message=""):
+    Exception.__init__(self)
+    self.message = message
+  def to_dict(self):
+    rv = {"code" : self.code,
+        "error" : errors[ERROR_USER_CANCELLED],
+        "info" : self.message}
+    return rv
+
+class GeocoderError(Exception):
+  """Exception raised for errors in geocoding service.
+  Attributes:
+    msg  -- server message
+  """
+  status_code = 500
+  code = ERROR_GEOCODER
+  def __init__(self, message=""):
+    Exception.__init__(self)
+    self.message = message
+  def __str__(self):
+    return self.message
+  def to_dict(self):
+    rv = {"code" : self.code,
+        "error" : errors[ERROR_GEOCODER],
+        "info" : self.message}
+    return rv
+
+def jsonToException(d):
+  if d["code"] == ERROR_INTERNAL:
+    return InternalError(d["info"])
+  elif d["code"] == ERROR_USER_CANCELLED:
+    return UserCancelled(d["info"])
+  elif d["code"] == ERROR_GEOCODER:
+    return GeocoderError(d["info"])
+  else:
+    return InvalidAPIUsage(d["info"],d["code"])
